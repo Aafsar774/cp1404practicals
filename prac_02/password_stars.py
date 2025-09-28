386d7565ff44ad122fa026aae593180dd3a7b381
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-09-28 20:56:02 +1000

    Add password check program using functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,17 @@
 character_length = 8
+def main():
+    password = get_password(character_length)
+    print_asterisks(password)
 
-password = input(f"Enter a password (minimum {character_length} characters): ")
-while len(password) < character_length:
-    print(f"Password must be at least {character_length} characters!")
-    password = input("Enter a password: ")
+def get_password(character_length):
+    password = input(f"Enter a password (minimum {character_length} characters): ")
+    while len(password) < character_length:
+        print(f"Password must be at least {character_length} characters!")
+        password = input("Enter a password: ")
+    return password
 
-print('*' * len(password))
+def print_asterisks(password):
+    print('*' * len(password))
+
+main()
+
