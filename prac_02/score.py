81182151e5edaf0f12c6758ce13ddc578769f1e9
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-09-28 22:23:43 +1000

    Add score status program using functions with random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,11 +3,14 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
+import random
 def main():
     score = float(input("Enter score: "))
     result = score_status(score)
     print(result)
 
+    random_score = random.uniform(0,100)
+    print(f"Random score: {random_score:.1f}\n{score_status(random_score)}")
 
 def score_status(score):
     if score < 0 or score > 100:
