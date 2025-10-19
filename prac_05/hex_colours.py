7bf23367d7f4aeed068be1b1aa2c573119ba9f1c
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-10-19 19:19:53 +1000

    add input loop that ends on blank

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,16 +1,16 @@
-from prac_04.list_comprehensions import names
-
 colour_names = {"Absolute Zero": "0048ba","Acid Green": "#b0bf1a","AliceBlue": "#f0f8ff","Alizarin crimson": "#e32636","Amaranth": "#e52b50","Amber": "#ffbf00","Amethyst": "#9966cc","AntiqueWhite": "#faebd7","AntiqueWhite1": "#ffefdb","AntiqueWhite2": "#eedfcc"}
-
 
 name_lower = {name.lower(): code for name, code in colour_names.items()}
 
 def main():
-    name = input("Enter colour name: ").strip()
-    key = name.lower()
-    if key in name_lower:
-        print(f"{name} = {name_lower[key]}")
-    else:
-        print("Invalid colour name")
+    while True:
+        name = input("Enter colour name: ").strip()
+        if not name:  # blank ends the loop
+            break
+        key = name.lower()
+        if key in name_lower:
+            print(f"{name} = {name_lower[key]}\n")
+        else:
+            print("Invalid colour name.Try again!\n")
 
 main()
