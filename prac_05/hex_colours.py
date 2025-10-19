c8ac87efdbeccf98f008d487d82cbd9abe7a8a31
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-10-19 19:08:14 +1000

    use .lower() to support case insensitive colour name lookup

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,16 @@
+from prac_04.list_comprehensions import names
+
 colour_names = {"Absolute Zero": "0048ba","Acid Green": "#b0bf1a","AliceBlue": "#f0f8ff","Alizarin crimson": "#e32636","Amaranth": "#e52b50","Amber": "#ffbf00","Amethyst": "#9966cc","AntiqueWhite": "#faebd7","AntiqueWhite1": "#ffefdb","AntiqueWhite2": "#eedfcc"}
 
-name = input("Enter colour name: ")
-if name in colour_names:
-    print(f"{name} -> {colour_names[name]}")
-else:
-    print("Invalid colour name")
+
+name_lower = {name.lower(): code for name, code in colour_names.items()}
+
+def main():
+    name = input("Enter colour name: ").strip()
+    key = name.lower()
+    if key in name_lower:
+        print(f"{name} = {name_lower[key]}")
+    else:
+        print("Invalid colour name")
+
+main()
