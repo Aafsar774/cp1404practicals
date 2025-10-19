3b08a60cb1acde47f6e33988b0afb01ee8cfec0e
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-10-19 22:40:36 +1000

    count champion wins and print

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -11,8 +11,20 @@ def main():
     with open(FILENAME, "r", encoding="utf-8-sig", newline="") as in_file:
         reader = csv.reader(in_file)
         header = next(reader)
-        rows = list(reader)            # list of lists
+        rows = list(reader)
 
-    print(rows , header)
+    champion_idx = header.index("Champion")
+
+    champion_to_wins = {}
+    for row in rows:
+        champion = row[champion_idx]
+        if champion in champion_to_wins:
+            champion_to_wins[champion] = champion_to_wins[champion] + 1
+        else:
+            champion_to_wins[champion] = 1
+
+    print("Wimbledon Champions :")
+    for champion, wins in champion_to_wins.items():
+        print(f"{champion} {wins}")
 
 main()
