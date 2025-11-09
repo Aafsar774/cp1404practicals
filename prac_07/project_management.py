47326cc47c3874a0175d92af9a9587e7b5b52fd0
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-09 22:20:32 +1000

    Add  menu loop with placeholders

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,11 +3,34 @@ from prac_07.project import Project
 
 text_file = "projects.txt"
 
+
 def main():
     print("Welcome to Pythonic Project Management")
     projects = load_projects(text_file)
     print(f"Loaded {len(projects)} projects from {text_file}")
-    print(projects)
+
+    while True:
+
+         print_menu()
+         choice = input(">>> ").strip().lower()
+         if choice == "q":
+             print("Thank you for using custom-built project management software.")
+             break
+         elif choice == "l":
+             print("Load projects (not implemented yet)")
+         elif choice == "s":
+             print("Save projects (not implemented yet)")
+         elif choice == "d":
+             print("Display projects (not implemented yet)")
+         elif choice == "f":
+             print("Filter by date (not implemented yet)")
+         elif choice == "a":
+             print("Add new project (not implemented yet)")
+         elif choice == "u":
+             print("Update project (not implemented yet)")
+         else:
+             print("Invalid menu choice.")
+
 
 def load_projects(filename):
     projects = []
@@ -23,6 +46,16 @@ def load_projects(filename):
             completion_percentage = int(parts[4].strip())
             projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
     return projects
+
+
+def print_menu():
+    print("- (L)oad projects")
+    print("- (S)ave projects")
+    print("- (D)isplay projects")
+    print("- (F)ilter projects by date")
+    print("- (A)dd new project")
+    print("- (U)pdate project")
+    print("- (Q)uit")
 
 
 main()
