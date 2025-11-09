7a5f6aafbb771abbeaf8801bcd78e92d0a65951d
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-09 21:10:08 +1000

    Add user input for new guitars and display new list

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,12 +2,20 @@ from prac_07.guitar import Guitar
 
 csv_file = "guitars.csv"
 
+
 def main():
     guitars = load_csv(csv_file)
     loaded_guitars(guitars, "Loaded guitars:")
 
     guitars.sort()
     loaded_guitars(guitars, "Guitars sorted by year from oldest to newest:")
+
+
+    new_guitars = add_guitars_from_user()
+    if new_guitars:
+        guitars.extend(new_guitars)
+        guitars.sort()
+        loaded_guitars(guitars, "All guitars including new entries:")
 
 def load_csv(filename):
     guitars = []
@@ -21,11 +29,24 @@ def load_csv(filename):
             guitars.append(Guitar(name=name, year=int(year), cost=float(cost)))
     return guitars
 
+
 def loaded_guitars(guitars, title):
     print(title)
     for item in guitars:
         print(item)
     print()
 
+
+def add_guitars_from_user():
+    print("Enter new guitars:")
+    new_guitars = []
+    name = input("Name: ").strip()
+    year = int(input("Year: ").strip())
+    cost = float(input("Cost: $").strip())
+    more_guitars = Guitar(name=name, year=year, cost=cost)
+    new_guitars.append(more_guitars)
+    print(f"{more_guitars} added.\n")
+
+    return new_guitars
 
 main()
