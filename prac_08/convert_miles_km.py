a9b389d2d7ca0d09b69f7638cb72ca0b931ee105
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-16 15:32:17 +1000

    Add miles to km conversion  and update output via StringProperty

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -3,12 +3,24 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty
 
 class ConvertMilesKilometre(App):
-    output = StringProperty()
+    output = StringProperty("0.0")
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
+
     def handle_update(self):
-        """Handle changes to the text input by updating the model from the view."""
         self.output = self.root.ids.input_miles.text
+
+    def handle_convert(self):
+        miles = self.get_miles()
+        kilometres = miles * 1.60934
+        self.output = str(kilometres)
+
+    def get_miles(self):
+        try:
+            return float(self.root.ids.input_miles.text)
+        except ValueError:
+            return 0.0
+
 ConvertMilesKilometre().run()
