8e7080040b2d5d900784903ec62b7da7e17f6516
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-16 15:09:23 +1000

    Wire kilometres output label to  mvc StringProperty model

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 class ConvertMilesKilometre(App):
-
+    output = StringProperty()
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file("convert_miles_km.kv")
         return self.root
-
-
+    def handle_update(self):
+        """Handle changes to the text input by updating the model from the view."""
+        self.output = self.root.ids.input_miles.text
 ConvertMilesKilometre().run()
