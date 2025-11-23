c46e7299e9d7bebc0d2c90ff2e44bdb0ee858739
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-23 22:30:45 +1000

    Implement drive_taxi to drive selected taxi and return fare and add active taxi selection and fare calculation to taxi_simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,20 +7,26 @@ def main():
     print("Let's drive!")
     taxis = [Taxi("Prius", 100),SilverServiceTaxi("Limo", 100, 2),SilverServiceTaxi("Hummer", 200, 4)]
     bill = 0.0
+    active_taxi = 0
 
     choice = input(f"{MENU}\n>>> ").lower()
     while choice != "q":
         if choice == "c":
-            choose_taxi(taxis)
+            chosen_taxi = choose_taxi(taxis)
+            if chosen_taxi:
+                active_taxi = chosen_taxi
         elif choice == "d":
-            print("You need to choose a taxi before you can drive")
+            if active_taxi==0:
+                print("You need to choose a taxi before you can drive")
+            else:
+                bill += drive_taxi(active_taxi)
         else:
             print("Invalid option")
 
-        print(f"Bill to date: ${bill}")
+        print(f"Bill to date: ${bill:.2f}")
         choice = input(f"{MENU}\n>>> ").lower()
 
-    print(f"Total trip cost: ${bill}")
+    print(f"Total trip cost: ${bill:.2f}")
     print("Taxis are now:")
     display_taxis(taxis)
 
@@ -35,16 +41,27 @@ def choose_taxi(taxis):
     display_taxis(taxis)
     try:
         taxi_choice = int(input("Choose taxi: "))
-        if 0 <= taxi_choice < len(taxis):
-            taxi = taxis[taxi_choice]
-            taxi.start_fare()
-            return taxi
+        if taxi_choice < 0 or taxi_choice >= len(taxis):
+            print("Invalid taxi choice")
+            return 0
+        taxi = taxis[taxi_choice]
+        taxi.start_fare()
+        return taxi
     except ValueError:
         print("Invalid taxi choice")
-        return None
+        return 0
 
-    print("Invalid taxi choice")
-    return None
+def drive_taxi(taxi):
+    try:
+        drive_distance = float(input("Drive how far? "))
+    except ValueError:
+        print("Invalid distance")
+        return 0.0
+
+    taxi.drive(drive_distance)
+    trip_cost = taxi.get_fare()
+    print(f"Your {taxi.name} trip cost you ${trip_cost:.2f}")
+    return trip_cost
 
 
 if __name__ == "__main__":
