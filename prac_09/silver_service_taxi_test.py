6eb1139c16c67f748b7b3824d50eb8e0be34ebbf
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-23 17:33:12 +1000

    Add SilverServiceTaxi subclass and scale price_per_km by fanciness and show flagfall in __str__

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,8 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    taxi = SilverServiceTaxi("Hummer", 200, fanciness=4)
+    print(taxi)
+
+if __name__ == "__main__":
+    main()
