7c0b949442213090f79e9620d092f34ffc556252
Aafsar_Iqbal <aafsar.iqbal@my.jcu.edu.au>
2025-11-30 15:06:53 +1000

    Add assert tests for Car fuel initialisation and enable doctest

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -46,13 +46,21 @@ def run_tests():
     # using the value passed in or the default
     # You should test both of these
     car = Car(fuel=10)
+    car.drive(3)
+    assert car.fuel == 7, "Fuel should decrease after driving"
+
+    car = Car()
+    assert car.fuel == 0, "Starting fuel should be 0"
+
+    car2 = Car(20)
+    assert car2.fuel == 20, "New fuel should be set correctly"
 
 
 run_tests()
 
 # TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
 # TODO: 4. Fix the failing is_long_word function
 # (Don't change the tests, change the function!)
